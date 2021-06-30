b151bc63a216c5ab450cdd0d986f2b3560f3b29c
moein <firouzi.m.moein@gmail.com>
2021-07-01 00:35:42 +0430

    change URLs

--- lookup/urls.py
+++ lookup/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-   path('', views.home, name="home")
+   path('', views.home, name="home"),
+   path('about/', views.about, name="about")
 ]
